315540af3195a07bc300e4551b5530f14cba0d45
Antoni March Marti <marchmarti.antoni@gmail.com>
2019-11-15 17:24:39 +0100

    eliminated comentaries

--- src/phi.js
+++ src/phi.js
@@ -1,21 +1,8 @@
 
 
 function phi (matrix) {
-    /**
-     * matrix[1][1] YES octopus, YES X.
-     * matrix[0][0] NOT octopus, NOT X.
-     * matrix[1][0] YES octopus, NOT X.
-     * matrix[0][1] NOT octopus, YES X.
-     */
 
      const DIVIDE =  matrix[1][1] * matrix[0][0] - matrix[1][0] * matrix[0][1]
-        /**
-     * Square root of:
-     * - matrix[1][0] + matrix[1][1] --> Whenever octopus is TRUE.
-     * - matrix[0][0] + matrix[0][1] --> Whenever octopus is FALSE.
-     * - matrix[0][1] + matrix[1][1] --> Whenever X is TRUE.
-     * - matrix[0][0] + matrix[1][0] --> Whenever X is FALSE.
-     */
 
      const DIVISOR = Math.sqrt(
          (matrix[1][0] + matrix[1][1]) * 
