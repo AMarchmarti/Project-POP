541aeaf65211b53e97ed8e7dc3d960dc215501c8
Carlos <patezno97@gmail.com>
2019-11-28 18:14:55 +0100

    header y footer funcionan

---
+++ src/app/components/shared/footer/footer.component.js
@@ -0,0 +1,26 @@
+(function() {
+    const currentDocument = document.currentScript.ownerDocument;
+
+    class FooterComponent extends HTMLElement {
+
+        constructor() {
+            super();
+        }
+
+        // Called when element is inserted in DOM
+        connectedCallback() {
+            const shadowRoot = this.attachShadow({ mode: 'open' });
+
+            // Select the template and clone it. Finally attach the cloned node to the shadowDOM's root.
+            // Current document needs to be defined to get DOM access to imported HTML
+            const template = currentDocument.querySelector('#footer-template');
+            const instance = template.content.cloneNode(true);
+            shadowRoot.appendChild(instance);
+
+            // Extract the attribute user-id from our element.
+            // Note that we are going to specify our cards like:
+            // <user-card user-id="1"></user-card>
+        }
+    }
+    customElements.define('app-footer', FooterComponent);
+})();
