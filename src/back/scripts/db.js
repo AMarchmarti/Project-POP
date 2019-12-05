f7fc8c16eb6d43da1db870e0a749cee7277de4aa
Antoni March Marti <marchmarti.antoni@gmail.com>
2019-12-05 22:59:00 +0100

    connected with db will be added

---
+++ src/back/scripts/db.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const url = 'http://ec2-15-188-56-148.eu-west-3.compute.amazonaws.com/pulpo/read.php'
+
+function getData() {
+    let diary = []
+    fetch(url)
+    .then (function (response) {
+        response.json()
+        .then( function(json){
+            JSON.parse(json[0].diario).forEach(element => {
+                diary.push(element)
+            });
+        })
+    })
+
+return diary
+}
+
+module.exports = getData()
