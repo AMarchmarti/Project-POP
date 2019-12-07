3b70f6d33906ccd0210e2af7984b5636bd5e8829
Antoni March Marti <marchmarti.antoni@gmail.com>
2019-12-07 19:10:44 +0100

    Calendar modified

--- src/app/components/calendar.js
+++ src/app/components/calendar.js
@@ -1,7 +1,7 @@
 'use strict'
 /**
  * We ask our document to give us the html component with the id that we request through:
- * @param table_component
+ * @param calendar_component
  * @document
  * @function @getElementById
  */
@@ -24,45 +24,45 @@ const Calendar = Object.create(Object);
  * @TableInfo -> object that contains the project functionalities
  * @modal -> Extern component
  */
-Calendar.calendarDiv = document.createElement('div')
-Calendar.calendarDiv.setAttribute('id', 'calendar')
+
 Calendar.TableInfo = require('../../back/scripts/index')
 Calendar.modal = require('./modal')
 
-/**
- * Now we will create the protopides of the object, previously created
- * @constructorCalendar
- * @buttonModal
- * @days
- */
-Calendar.prototype.constructorCalendar = function constuctorCalendar(element, index){
+
+Calendar.prototype.createCalendar = function createCalendar(events){
     /**
-     * This is the functionality of creating the calendar, that is to say the 
-     * calendar and that makes every day of the week, it is divided into different parts to be able 
-     * to make the code more readable and easier to maintain
-     * we created in this function:
-     * @param element
-     * @param index
-     * @modal_scene -> is a div that contains all the buttons and modals of the page
-     * @buttonOpen -> Use another funtion of the object prototype to create the button to open modals
-     * @setAttribute -> is a function to modified the elements from the DOM and add the attributes that we need
-     * @modal -> it is a property of the object that in turn is an external component (Modal object) 
-     * then we use the function 
-     * @appendChild
-     * to unify the components in the 
-     * @calendar property
+     * Function for the creation of the calendar, divided by months containing 
+     * the name of the days of the week and the main and div container of the days of the month
+     * @param events -> array that contains the events of days and reset every 30 days
+     * @weekDays -> Array with the name of the days of week
      */
-    let modal_scene = document.createElement('div') 
-    modal_scene.setAttribute('class', 'modal_scene') 
-    let buttonOpen = this.buttonModal(index) 
-    let modal =  this.modal.constructorModal(element, index, this.TableInfo)
+    let main = document.createElement('main')
+    const weekDays  =['Lun','Mar','Mié','Jue','Vie','Sáb','Dom']
+    
+    let divWeek = document.createElement('div')
+    divWeek.setAttribute('class', 'day-of-week')
+    
+    let divCalendar = document.createElement('div')
+    divCalendar.setAttribute('class', 'calendar')
+    
+    let div  = document.createElement('div')
+    div.setAttribute('class', 'date-grid')
 
-    // this.closeModalWindow(modal)
-    modal_scene.appendChild(modal)
-    modal_scene.appendChild(buttonOpen)
-    this.calendarDiv.appendChild(modal_scene)
+    events.forEach( event => {
+        div.appendChild(event)
+    })
+
+    weekDays.forEach( day => {
+        let divDay  = document.createElement('div')
+        divDay.textContent = day
+        divWeek.appendChild(divDay)
+        
+    })
+    divCalendar.appendChild(divWeek)
+    divCalendar.appendChild(div)
+    main.appendChild(divCalendar)
+    calendar.appendChild(main)
 }
-
 Calendar.prototype.buttonModal = function showModal(index){
     /**
      * Here we create the button to activate the manners, it has as a link element, 
@@ -73,10 +73,18 @@ Calendar.prototype.buttonModal = function showModal(index){
     let buttonOpen = document.createElement('a')
     buttonOpen.setAttribute('href', `#openModal${index}`)
     buttonOpen.setAttribute('class', 'btn')
-    index < 10 
-        ? buttonOpen.textContent = `Dia 0${index + 1}` 
-        : buttonOpen.textContent = `Dia ${index + 1}`
+    let button = document.createElement('button')
+   if (index < 9){
+        button.textContent = `0${index + 1}`
+   } else if (9 < index && index < 30){
+        button.textContent = `${index + 1}`
+   } else {
+       (index % 30) < 9
+        ? button.textContent = `0${(index % 30) + 1}`
+        : button.textContent = `${(index % 30)+ 1}`
+   }
     
+    buttonOpen.appendChild( button)
     return buttonOpen
 }
 // TODO
@@ -88,35 +96,46 @@ Calendar.prototype.buttonModal = function showModal(index){
     
 // }
 
-Calendar.prototype.days = function showDays() {
+Calendar.prototype.constructorCalendar = function showDays() {
     /**
      * Here we go through the diary to obtain each day, 
      * both the number of the day and the itinerary it does
      * we create a constant that refers to the newspaper that 
      * we have already worked
+     * This is the functionality of creating the calendar, that is to say the 
+     * calendar and that makes every day of the week, it is divided into different parts to be able 
+     * to make the code more readable and easier to maintain
+     * we created in this function:
      * @DIARY
-     * and we use the function 
-     * @constructorTableDiary
-     * we have explained before to build the calendar
+     * and we cretae an array that keep the buttons of one month
+     * @events
+     * The we construct the diferents parts of modal:
+     * @function this.modal.constructorModal(@param DIARY[index], @param index, @param TableInfo)
+     * @function push -> keep the button that we create with @function this.buttonModal
+     * and every month reset events, we create de div of days, tha main part and the days of weexk. And
+     * we put it all together in calendar
+     * @function this.createCalendar
+     * 
      */
+    let events = []
     const DIARY = this.TableInfo.recolectInfo.diary;
     for(let index = 0; index <= DIARY.length - 1; index++){
-        this.constructorCalendar(DIARY[index], index)
+        let modal = this.modal.constructorModal(DIARY[index], index, this.TableInfo)
+        calendar.appendChild(modal)
+        events.push(this.buttonModal(index))
+        if((index + 1) % 30 === 0 ){
+           this.createCalendar(events)
+            events = []
+        }
     }
 }
 
 /**
- * Almost at the end we call the function days that will create the calendar 
+ * Almost at the end we call the function constructorCalendar that will create the calendar 
  * with the object we have created
  * @Calendar
- * @days
+ * @constructorCalendar
  */
-Calendar.days()
+Calendar.constructorCalendar()
 
-/**
- * Finally we add the object created to the main table that we had already named 
- * at the beginning of the file
- * @calendar
- */
-
-calendar.appendChild(Calendar.calendarDiv) 
+ 
