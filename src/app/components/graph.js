e0b4de0385fa2033d42b3d8f0e37ce54fe1ce902
Antoni March Marti <marchmarti.antoni@gmail.com>
2019-12-08 23:42:44 +0100

    fix tooltip of graph

--- src/app/components/graph.js
+++ src/app/components/graph.js
@@ -63,10 +63,8 @@ Graph.prototype.createGraph = function create() {
   this.info.sortTable().forEach(row => {
     let newPhi = row.phi > 0 ? row.phi : row.phi * -1;
     let div = document.createElement("div");
-    let span = document.createElement("span");
 
-    span.setAttribute("data-md-tooltip", `Evento: ${row.item} Probabilidad: ${(row.phi * 100).toPrecision(4)}%`);
-    div.appendChild(span)
+    div.setAttribute("data-md-tooltip", `Evento: ${row.item} Probabilidad: ${(row.phi * 100).toPrecision(4)}%`);
     div.setAttribute("class", "bar");
     div.setAttribute("style", `--bar-value:${newPhi * 100}%`);
     Graph.divGraph.appendChild(div);
